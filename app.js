4f5e832f7d7f95d35e5ee2fa4c924d17c3c5379f
Tyson Tran <tysontran@Google-Nest-Mini.hitronhub.home>
2023-05-17 15:28:10 -0700

    able to add as many gifts as you want, added remove button

--- app.js
+++ app.js
@@ -3,20 +3,28 @@ console.log("Let's get this party started!");
 async function getMeme(search) {
   const url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`;
   const res = await axios.get(url);
-  const img = document.querySelector(".meme");
-  img.src = res.data.data[0].images.downsized.url;
+  const randomIndex = Math.floor(Math.random() * res.data.data.length);
+  const gifUrl = res.data.data[randomIndex].images.downsized.url;
+
+  const container = document.querySelector("#meme-container");
+  const newImg = document.createElement("img");
+  newImg.setAttribute("src", gifUrl);
+  container.append(newImg);
 }
 
 const form = document.querySelector("#memes");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
   const input = document.querySelector("#search-meme");
 
-  const image = document.createElement("img");
-  image.classList.add("meme");
   getMeme(input.value);
-  input.value = "";
 
-  const container = document.querySelector("#meme-container");
-  container.append(image);
+  input.value = "";
 });
+
+const removeButton = document.querySelector('#remove');
+removeButton.addEventListener('click', function () {
+    const container = document.querySelector("#meme-container");
+    container.innerHTML = ''
+})
